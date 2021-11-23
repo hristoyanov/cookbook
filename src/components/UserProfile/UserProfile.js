963555237553e1a4f464bd36f6e54867e180260a
hristoyanov <73996482+hristoyanov@users.noreply.github.com>
2021-11-23 22:22:59 +0200

    No idea.

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -16,10 +16,10 @@ const UserProfile = ({
                 setUserProfile(res);
             });
 
-        if ((currentUser && currentUser.uid) && userProfile.userUID && currentUser.uid === userProfile.userUID) {
+        if (currentUser && currentUser.uid === userProfile.userUID) {
             getUserRecipes(id)
                 .then(res => {
-                    setRecipes(res)
+                    setRecipes(res)               
                 })
                 .catch(error => console.log(error));
         } else {
@@ -34,7 +34,7 @@ const UserProfile = ({
     return (
         <section className="user-recipes">
             <h1 className="user-recipes-title">
-                {currentUser && currentUser.uid && userProfile.userUID ? 'My recipes' : userProfile.displayName ? `${userProfile.displayName}'s recipes` : null}
+                {currentUser && userProfile.userUID && currentUser.uid === userProfile.userUID ? 'My recipes' : `${userProfile.displayName}'s recipes`}
             </h1>
             {userProfile.displayName
                 ?
