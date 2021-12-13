cb8dbd7ae1cf20c517409df6bf478390ecfe983f
hristoyanov <73996482+hristoyanov@users.noreply.github.com>
2021-12-13 13:45:35 +0200

    Added input validation for recipe create/edit.

--- src/components/RecipeForm/RecipeForm.js
+++ src/components/RecipeForm/RecipeForm.js
@@ -4,6 +4,7 @@ import AlertWindow from '../common/AlertWindow/AlertWindow';
 import isAuth from '../../hoc/isAuth';
 import { useAuthContext } from '../../contexts/AuthContext';
 import { getRecipe, addRecipe, editRecipe } from '../../services/services';
+import * as validators from './recipeValidators';
 
 import './RecipeForm.css';
 
@@ -20,6 +21,7 @@ const RecipeForm = ({
     const [showAlertWindow, setShowAlertWindow] = useState(false);
     const [alertWindowMessage, setAlertWindowMessage] = useState('');
     const [recipeId, setRecipeId] = useState('');
+    const [errors, setErrors] = useState({ name: false, imageURL: false, ingredients: false, prepTime: false, preparation: false })
 
     useEffect(() => {
         if (mode === 'Edit') {
@@ -39,12 +41,52 @@ const RecipeForm = ({
     async function onSubmitHandler(e) {
         e.preventDefault();
 
-        const name = e.target.name.value.trim();
-        const imageURL = e.target.imageURL.value.trim();
-        const ingredients = e.target.ingredients.value.split(',').map(x => x.trim());
-        const prepTime = Number(e.target.prepTime.value.trim());
-        const preparation = e.target.preparation.value.trim();
+        const name = validators.nameValidator(e.target.name.value.trim());
+        const imageURL = validators.urlValidator(e.target.imageURL.value.trim());
+        const ingredients = validators.ingredientsValidator(e.target.ingredients.value.split(',').map(x => x.trim()));
+        const prepTime = validators.prepTimeValidator(Number(e.target.prepTime.value.trim()));
+        const preparation = validators.preparationValidator(e.target.preparation.value.trim());
         const hidden = e.target.visibility.value === 'private' ? true : false;
+
+        if (!name) {
+            setErrors(state => ({ ...state, name: 'Name should be at least 3 characters long.' }));
+
+            return;
+        } else {
+            setErrors(state => ({ ...state, name: false }));
+        }
+
+        if (!imageURL) {
+            setErrors(state => ({ ...state, imageURL: 'Please enter a valid URL.' }));
+
+            return;
+        } else {
+            setErrors(state => ({ ...state, imageURL: false }));
+        }
+
+        if (!ingredients) {
+            setErrors(state => ({ ...state, ingredients: 'Please enter at least 3 different ingredients.' }));
+
+            return;
+        } else {
+            setErrors(state => ({ ...state, ingredients: false }));
+        }
+
+        if (!prepTime) {
+            setErrors(state => ({ ...state, prepTime: 'Preparation time can\'t be less than 5 minutes and greater than 1440 minutes (24 hours).' }));
+
+            return;
+        } else {
+            setErrors(state => ({ ...state, prepTime: false }));
+        }
+
+        if (!preparation) {
+            setErrors(state => ({ ...state, preparation: 'Preparation description must be at least 10 characters.' }));
+
+            return;
+        } else {
+            setErrors(state => ({ ...state, preparation: false }));
+        }
 
         if (mode === 'Add') {
             const ownerId = user.uid;
@@ -103,15 +145,20 @@ const RecipeForm = ({
                 <form className="recipe-form" onSubmit={onSubmitHandler}>
                     <legend>{mode + ' recipe'}</legend>
                     <label htmlFor="recipe-name">Recipe Name</label>
-                    <input type="text" name="name" id="recipe-name" defaultValue={loaded ? recipe.name : ''} required />
+                    <input type="text" name="name" id="recipe-name" defaultValue={loaded ? recipe.name : ''}/>
+                    <span className={errors.name ? 'error visible' : 'error'}>{errors.name}</span>
                     <label htmlFor="image-url">Image</label>
-                    <input type="text" name="imageURL" id="image-url" placeholder="Please provide image source." defaultValue={loaded ? recipe.imageURL : ''} required />
+                    <input type="text" name="imageURL" id="image-url" placeholder="Please provide image source." defaultValue={loaded ? recipe.imageURL : ''}/>
+                    <span className={errors.imageURL ? 'error visible' : 'error'}>{errors.imageURL}</span>
                     <label htmlFor="ingredients">Ingredients</label>
-                    <textarea name="ingredients" id="ingredients" cols="30" rows="7" placeholder="Please add ingredients separated by comma." defaultValue={loaded ? recipe.ingredients.join(', ') : ''} required></textarea>
+                    <textarea name="ingredients" id="ingredients" cols="30" rows="7" placeholder="Please add ingredients separated by comma." defaultValue={loaded ? recipe.ingredients.join(', ') : ''}></textarea>
+                    <span className={errors.ingredients ? 'error visible' : 'error'}>{errors.ingredients}</span>
                     <label htmlFor="prep-time">Preparation time (in minutes)</label>
-                    <input type="number" name="prepTime" id="prep-time" min="0" defaultValue={loaded ? recipe.prepTime : ''} required />
+                    <input type="number" name="prepTime" id="prep-time" min="5" defaultValue={loaded ? recipe.prepTime : ''}/>
+                    <span className={errors.prepTime ? 'error visible' : 'error'}>{errors.prepTime}</span>
                     <label htmlFor="preparation">Preparation</label>
-                    <textarea name="preparation" id="preparation" cols="30" rows="7" defaultValue={loaded ? recipe.preparation : ''} required></textarea>
+                    <textarea name="preparation" id="preparation" cols="30" rows="7" defaultValue={loaded ? recipe.preparation : ''} placeholder="Instructions as to how to prepare the recipe."></textarea>
+                    <span className={errors.preparation ? 'error visible' : 'error'}>{errors.preparation}</span>
                     <label htmlFor="visibility">This recipe should be:</label>
                     <select type="text" name="visibility" id="visibility" defaultValue={!loaded ? null : recipe.hidden ? 'private' : 'public'}>
                         <option value="public">Public</option>
---
+++ src/components/RecipeForm/recipeValidators.js
@@ -0,0 +1,21 @@
+export const nameValidator = (name) => {
+    return name.length > 2 && name.length <= 50 ? name : false;
+}
+
+export const urlValidator = (url) => {
+    return url.startsWith('https://') ? url : false;
+}
+
+export const ingredientsValidator = (array) => {
+    const uniqueIngredients = new Set(array);
+
+    return uniqueIngredients.size > 2 ? Array.from(uniqueIngredients) : false;
+}
+
+export const prepTimeValidator = (number) => {
+    return number > 4 && number <= 1440 ? number : false;
+}
+
+export const preparationValidator = (string) => {
+    return string.length > 9 ? string : false;
+}
